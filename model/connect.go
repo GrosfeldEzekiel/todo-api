7c20ff2d1285d3845cfb232c7865f131c5663253
ezekiel <grosfeldezekel@gmail.com>
2020-10-05 20:50:28 -0300

    Adding database url as env

--- model/connect.go
+++ model/connect.go
@@ -4,13 +4,15 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 )
 
 var con *sql.DB
 
 
 func Connect () *sql.DB {
-	db, err := sql.Open("mysql", "JYalzdGUu9:uBRpLtlFL0@tcp(remotemysql.com:3306)/JYalzdGUu9")
+	url := os.Getenv("DATABASE_URL")
+	db, err := sql.Open("mysql", url)
 	if err != nil {
 		log.Fatal(err)
 	}
