712b7828cd80ecae29566351eeee3767af8083aa
ezekiel <grosfeldezekel@gmail.com>
2020-10-05 20:10:33 -0300

    heroku2

--- main.go
+++ main.go
@@ -7,15 +7,18 @@ import (
 	"log"
 	"net/http"
 	_ "github.com/go-sql-driver/mysql"
+	"os"
 )
 
 func main() {
 	mux := controller.Register()
+
+	port := os.Getenv("PORT")
 
 	db := model.Connect()
 	defer db.Close() // Run the code at the end
 
 	fmt.Println("Serving...")
 
-	log.Fatal(http.ListenAndServe(":3000", mux))
+	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
