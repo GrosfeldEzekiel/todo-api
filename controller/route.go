e671dbdb35f45535f5365d761f2351861ba9a38c
ezekiel <grosfeldezekel@gmail.com>
2020-10-12 18:29:45 -0300

    new structure

--- controller/route.go
+++ controller/route.go
@@ -1,12 +1,21 @@
 package controller
 
 import (
+	"log"
 	"net/http"
 )
 
+// Register initialize mux
 func Register() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/ping", ping())
 	mux.HandleFunc("/", crud())
 	return mux
+}
+
+// Web starts the web
+func Web() {
+	http.Handle("/ping", ping())
+	http.Handle("/", crud())
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- main.go
+++ main.go
@@ -4,21 +4,20 @@ import (
 	"LearningGo/section4/todo-api/controller"
 	"LearningGo/section4/todo-api/model"
 	"fmt"
-	"log"
-	"net/http"
+
 	_ "github.com/go-sql-driver/mysql"
-	"os"
 )
 
 func main() {
-	mux := controller.Register()
+	// mux := controller.Register()
 
-	port := os.Getenv("PORT")
+	// port := os.Getenv("PORT")
 
 	db := model.Connect()
 	defer db.Close() // Run the code at the end
 
 	fmt.Println("Serving...")
 
-	log.Fatal(http.ListenAndServe(":"+port, mux))
+	// log.Fatal(http.ListenAndServe(":"+port, mux))
+	controller.Web()
 }
